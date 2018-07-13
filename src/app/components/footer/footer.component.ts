1f56dc523b57b5ab4ee29223fc5a2184fd30d99d
Luis Eduardo Castillo Ramirez <lecastillo@gbm.com.mx>
2018-07-12 22:35:20 -0500

    TSLint improve code.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { InfoService } from "../../services/info.service"
+import { InfoService } from '../../services/info.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html'
 })
 export class FooterComponent {
-  
-  year:number = new Date().getFullYear();
 
-  constructor(public infoService:InfoService){
+  year: number = new Date().getFullYear();
+
+  constructor(public infoService: InfoService) {
 
   }
 }
--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Http } from "@angular/http"
-import { getLocaleDateFormat } from '../../../node_modules/@angular/common';
+import { Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
 
-  team:any = {};
-  loaded:boolean = false;
+  team: any = {};
+  loaded = false;
 
-  constructor(public http:Http) {
+  constructor(public http: Http) {
       this.loadTeam();
    }
 
-   public loadTeam(){
-    this.http.get("https://joyeriasmaria-web.firebaseio.com/team.json")
-    .subscribe(data =>{
+   public loadTeam() {
+    this.http.get('https://joyeriasmaria-web.firebaseio.com/team.json')
+    .subscribe(data => {
        this.team = data.json();
        this.loaded = true;
+       console.log(this.team);
     });
    }
 }
