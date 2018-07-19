27ea09cae7e0d638e09018976ed5bce652e4673c
Luis Castillo <luised.castillo@gmail.com>
2018-07-18 21:21:46 -0500

    Product page & component ready to show it.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
-import {RouterModule, Routes} from "@angular/router";
+import {RouterModule, Routes} from '@angular/router';
 
-import {AboutComponent, 
-        MainComponent, 
+import {AboutComponent,
+        MainComponent,
         ProductItemComponent
-    } from "./components/index.pages";
+    } from './components/index.pages';
 
 const app_routes: Routes = [
-    { path: "home", component: MainComponent },
-    { path: "about", component:AboutComponent },
-    { path: "product", component:ProductItemComponent },
-    { path: "**", pathMatch: "full", redirectTo: "home" }
+    { path: 'home', component: MainComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'product/:id', component: ProductItemComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
-export const AppRouting = RouterModule.forRoot(app_routes);
+export const AppRouting = RouterModule.forRoot(app_routes, {useHash: true});
--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
 })
 export class ProductItemComponent {
+
+  constructor(private route: ActivatedRoute) {
+    route.params.subscribe(params => {
+      console.log(params);
+      console.log(params['id']);
+    });
+  }
 
 }
